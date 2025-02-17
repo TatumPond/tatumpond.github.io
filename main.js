b6b870266a382bd1dd4c03900484528224d1ace8
TatumPond <127900772+TatumPond@users.noreply.github.com>
2025-02-17 12:43:02 -0700

    javascript added

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function openPopup(event) {
+    event.preventDefault();
+    document.getElementById('projectPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('projectPopup').style.display = 'none';
+}
