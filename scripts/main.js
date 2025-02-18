b659559b9318de94b826f3fbacc3834993330b75
TatumPond <127900772+TatumPond@users.noreply.github.com>
2025-02-18 10:45:35 -0700

    added make a wish project

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 
 function openPopup(event) {
     event.preventDefault();
-    document.getElementById('projectPopup').style.display = 'block';
+    document.getElementById('FishAndGamePopup').style.display = 'block';
 }
 
 function closePopup() {
-    document.getElementById('projectPopup').style.display = 'none';
+    document.getElementById('FishAndGamePopup').style.display = 'none';
 }
